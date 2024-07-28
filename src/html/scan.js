b4b899867a7e307b747108c1a05c05283f0a78ab
Bryce Allen <bryce@bda.space>
2024-07-28 17:47:52 -0400

    fix pwm table column headers

--- src/html/scan.js
+++ src/html/scan.js
@@ -230,7 +230,7 @@ function updatePwmSettings(arPwm) {
   var pinTxIndex = undefined;
   var pinModes = []
   // arPwm is an array of raw integers [49664,50688,51200]. 11 bits of failsafe position, 4 bits of input channel, 1 bit invert, 4 bits mode, 1 bit for narrow/750us
-  const htmlFields = ['<div class="mui-panel pwmpnl"><table class="pwmtbl mui-table"><tr><th class="fixed-column">Output</th><th class="mui--text-center fixed-column">Features</th><th>Mode</th><th>Input</th><th class="mui--text-center fixed-column">Invert?</th><th class="mui--text-center fixed-column">750us?</th><th>Failsafe</th><th class="mui--text-center fixed-column pwmitm">Failsafe Mode</th><th class="mui--text-center fixed-column pwmitm">Failsafe Pos</th></tr>'];
+  const htmlFields = ['<div class="mui-panel pwmpnl"><table class="pwmtbl mui-table"><tr><th class="fixed-column">Output</th><th class="mui--text-center fixed-column">Features</th><th>Mode</th><th>Input</th><th class="mui--text-center fixed-column">Invert?</th><th class="mui--text-center fixed-column">750us?</th><th>Failsafe Mode</th><th>Failsafe Pos</th><th class="mui--text-center fixed-column pwmitm">Limit Min</th><th class="mui--text-center fixed-column pwmitm">Limit Max</th></tr>'];
   // const htmlFields = ['<div class="mui-panel"><table class="pwmtbl mui-table"><tr><th class="fixed-column">Output</th><th class="mui--text-center fixed-column">Features</th><th>Mode</th><th>Input</th><th class="mui--text-center fixed-column">Invert?</th><th class="mui--text-center fixed-column">750us?</th><th>Failsafe</th><th>Limit Min</th><th>Limit Max</th></tr>'];
   arPwm.forEach((item, index) => {
     const failsafe = (item.config & 2047); // 11 bits
@@ -292,7 +292,7 @@ function updatePwmSettings(arPwm) {
           'ch13 (AUX9)', 'ch14 (AUX10)', 'ch15 (AUX11)', 'ch16 (AUX12)']);
     const failsafeModeSelect = enumSelectGenerate(`pwm_${index}_fsmode`, failsafeMode,
         ['Set Position', 'No Pulses', 'Last Position']); // match eServoOutputFailsafeMode
-    
+
     /*
     const inputSelect = enumSelectGenerate(`pwm_${index}_ch`, ch, channels);
     htmlFields.push(`<tr><td class="mui--text-center mui--text-title">${index + 1}</td>
